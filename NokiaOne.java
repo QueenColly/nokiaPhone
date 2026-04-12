6a4fd7343abf73ef3fcfd0f0e01e41517748b797
QueenColly <colleteumeh0@gmail.com>
2026-04-12 09:47:35 +0100

    first commit

---
+++ NokiaOne.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class NokiaOne { 
+public static void main (String[] args){
+Scanner inputCollector = new Scanner (System.in);
+//System.out.print("Welcome to Nokia!!!");
+//String input = inputCollector.nextLine();
+//
+    String Menu = """
+
+    Welcome your Nokia phone !!!!
+
+    Your Nokia menu map:
+
+    List of menu functions:
+
+        1. Phone book
+        2. Message 
+        3. Chat
+        4. Call register
+        5. Tones
+        6. Settings
+        7. Call divert
+        8. Games
+        9. Calculator
+       10. Reminders
+       11. Clock
+       12. Profiles
+       13. SIM services
+
+    """ ; 
+    System.out.println(Menu);  
+        
+
+}
+}
